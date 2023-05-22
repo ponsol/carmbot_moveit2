a885e1d039d53fdfd199103408fbfca3dbd456cc
sjk <jeyaks@gmail.com>
2023-05-22 20:59:39 +0000

    version 2; load_parameters

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
-# carmbot_ros2_moveit2
+# carmbot_moveit2
 
-C++ node for armbot_ros2_moveit2. 
+C++ node for armbot_moveit2. 
 This works only using a named pose. 
+
+
+colcon build --mixin debug --packages-select carmbot_moveit2
+
+ros2 run carmbot_moveit2 carmbot_moveit2
--- src/carmbot_moveit2.cpp
+++ src/carmbot_moveit2.cpp
@@ -1,5 +1,6 @@
 #include <cstdio>
 #include <memory>
+#include <chrono>
 
 #include <rclcpp/rclcpp.hpp>
 #include <moveit/move_group_interface/move_group_interface.h>
@@ -9,6 +10,7 @@ int main(int argc, char ** argv)
 
   printf("Starting carmbot_moveit2 package\n");
 
+
   rclcpp::init(argc, argv);
   auto const node = std::make_shared<rclcpp::Node>(
     "carmbot_moveit2",
@@ -17,32 +19,96 @@ int main(int argc, char ** argv)
   auto const logger = rclcpp::get_logger("carmbot_moveit2");
 
 
+#define SPIN
 
-  //spin the node
-  rclcpp::executors::SingleThreadedExecutor executor;
-  executor.add_node(node);
-  auto spinner = std::thread([&executor]() { executor.spin(); });
-  /*
-  */
+#ifdef SPIN
+     //spin the node
+     //rclcpp::executors::SingleThreadedExecutor executor;
+     rclcpp::executors::SingleThreadedExecutor executor;
+	   printf("ok here two\n");
+     executor.add_node(node);
+	   printf("ok here three\n");
+
+     auto spinner = std::thread([&executor]() { executor.spin(); });
+
+#endif
+
+
+
+
+#define LKIN
+
+#ifdef LKIN 
+	   printf("ok here 1\n");
+   // get all kinematics parameters from the move_group node
+    auto parameters_client = std::make_shared<rclcpp::SyncParametersClient>(node, "/move_group");
+	   printf("ok here 2\n");
+    auto plist = parameters_client->load_parameters("kinematics.yaml",  std::chrono::seconds(1)  );
+
+    //printf("%s\n", plist.parameters );
+     /*
+    while (!parameters_client->wait_for_service( std::chrono::seconds(1) )) {
+      if (!rclcpp::ok()) {
+        RCLCPP_ERROR(node->get_logger(), "Interrupted while waiting for the service. Exiting.");
+        rclcpp::shutdown();
+#ifdef SPIN
+        spinner.join();
+#endif	
+      }
+      RCLCPP_INFO(node->get_logger(), "service not available, waiting again...");
+    }
+    */
+
+
+
+	   printf("ok here 3\n");
+    //executor.remove_node(node);
+    rcl_interfaces::msg::ListParametersResult parameter_list = parameters_client->list_parameters({"robot_description_kinematics"},15);
+	   printf("ok here 3 1\n");
+    //printf("names %s\n", (char *) parameter_list.names );
+
+    //auto parameters = parameters_client->get_parameters(parameter_list.names);
+    //printf("names %s\n",  (char *) parameter_list.names );
+    // set the parameters to our node
+	   printf("ok here 4\n");
+    //node->set_parameters(parameters);
+	   printf("ok here 5\n");
+#endif
+
+
 
 
 
   // Create the MoveIt MoveGroup Interface
   using moveit::planning_interface::MoveGroupInterface;
   auto move_group_interface = MoveGroupInterface(node, "arm");
+  //auto move_group_interface = MoveGroupInterface(node, "panda_arm");
+
 
   // Set a target Pose
   auto const target_pose = []{
     geometry_msgs::msg::Pose msg;
-    //msg.orientation.w = 0.0;
-    msg.position.x = 0.11;
-    msg.position.y = 1.25;
-    msg.position.z = 0.0;
+    msg.orientation.x = 0.434;
+    msg.orientation.y = 0.237;
+    msg.orientation.z = 0.417;
+    msg.orientation.w = 0.763;
+    msg.position.x = -1.514;
+    msg.position.y =  0.806;
+    msg.position.z =  3.206;
+    return msg;
+  }();
+
+   auto const ptarget_pose = [] {
+    geometry_msgs::msg::Pose msg;
+    msg.orientation.w = 1.0;
+    msg.position.x = 0.28;
+    msg.position.y = -0.2;
+    msg.position.z = 0.5;
     return msg;
   }();
 
 
-
+  /*
    moveit::core::RobotState start_state(*move_group_interface.getCurrentState());
    move_group_interface.setStartState(start_state);
   //const moveit::core::JointModelGroup* joint_model_group =
@@ -50,17 +116,18 @@ int main(int argc, char ** argv)
   // start_state.setFromIK(joint_model_group, start_pose2);
   //move_group_interface.setStartState(target_pose);
 
-  /*
    */
 
+//  for (int i=0; i < 100000; i++ );
 
 
   printf("Before set target \n");
+  //move_group_interface.setPoseTarget(target_pose);
   //move_group_interface.setPoseTarget(target_pose, "eef_link");
-  //move_group_interface.setApproximateJointValueTarget(target_pose, "eef_link");
-  //move_group_interface.setApproximateJointValueTarget(target_pose);
-  //move_group_interface.setRPYTarget(0.17,0.0, 0); 
-  move_group_interface.setNamedTarget("pose1");
+  //move_group_interface.setApproximateJointValueTarget(target_pose, "panda_link8");
+  move_group_interface.setApproximateJointValueTarget(target_pose);
+  //move_group_interface.setRPYTarget(1.5,0.017, 0.017); 
+  //move_group_interface.setNamedTarget("pose1");
   printf("After set target \n");
 
   
@@ -80,7 +147,10 @@ int main(int argc, char ** argv)
   
   // Shutdown ROS
   rclcpp::shutdown();
-  spinner.join();
+
+#ifdef SPIN
+    spinner.join();
+#endif 
 
   return 0;
 }
