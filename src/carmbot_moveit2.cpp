3aa6618e2147a63160d7bbc74a66e24b3cd37c09
sjk <jeyaks@gmail.com>
2023-05-24 05:07:41 +0000

    version 3; params.yaml

--- src/carmbot_moveit2.cpp
+++ src/carmbot_moveit2.cpp
@@ -1,9 +1,87 @@
 #include <cstdio>
 #include <memory>
 #include <chrono>
+#include <vector>
+
 
 #include <rclcpp/rclcpp.hpp>
 #include <moveit/move_group_interface/move_group_interface.h>
+
+using moveit::planning_interface::MoveGroupInterface;
+
+auto const logger = rclcpp::get_logger("carmbot_moveit2");
+   
+
+void tprint( moveit::planning_interface::MoveGroupInterface::Plan &plan ) {
+
+  auto msg = plan.trajectory_;
+
+  std::vector<std::string> jnames = plan.trajectory_.joint_trajectory.joint_names ;
+
+  std::vector<float>::size_type tsize = msg.joint_trajectory.points.size() ;
+
+  printf("size of trajectory: %ld\n", tsize );
+
+  std::vector<double> tpos ;
+  for (unsigned i=0; i< tsize; i++)
+  {
+     tpos = msg.joint_trajectory.points[i].positions;
+     printf("position %d = %f %f %f\n", i, tpos[0], tpos[1], tpos[2] );
+  }
+
+}
+
+
+
+int plan_move ( moveit::planning_interface::MoveGroupInterface &move_group_interface,
+		std::vector<float> &pos, std::vector<float> &ang) {
+
+  int rval = 0;
+
+  // Set a target Pose
+  auto target_pose = [&pos, &ang]{
+    geometry_msgs::msg::Pose msg;
+    msg.orientation.x = ang[0];
+    msg.orientation.y = ang[1];
+    msg.orientation.z = ang[2];
+    msg.orientation.w = ang[3];
+    msg.position.x = pos[0];
+    msg.position.y = pos[1];
+    msg.position.z = pos[2];
+    return msg;
+  }();
+
+  printf("target position(xyz) %f %f %f\n", target_pose.position.x, target_pose.position.y, target_pose.position.z);
+
+  //moveit::core::RobotState start_state(*move_group_interface.getCurrentState());
+  //move_group_interface.setStartState(start_state);
+
+
+
+  move_group_interface.setApproximateJointValueTarget(target_pose);
+
+  // Create a plan to that target pose
+  auto  [success, plan] = [&move_group_interface]{
+    moveit::planning_interface::MoveGroupInterface::Plan msg;
+    auto ok = static_cast<bool>(move_group_interface.plan(msg));
+    return std::make_pair(ok, msg);
+  }();
+
+  tprint( plan);
+
+  // Execute the plan
+  if(success) {
+    move_group_interface.execute(plan);
+  } else {
+    RCLCPP_ERROR(logger, "Planing failed!");
+    rval = -1;
+  }
+
+  return rval;
+}
+
+
+
 
 int main(int argc, char ** argv)
 {
@@ -16,136 +94,31 @@ int main(int argc, char ** argv)
     "carmbot_moveit2",
     rclcpp::NodeOptions().automatically_declare_parameters_from_overrides(true)
   );
-  auto const logger = rclcpp::get_logger("carmbot_moveit2");
-
+  
 
 #define SPIN
 
 #ifdef SPIN
      //spin the node
-     //rclcpp::executors::SingleThreadedExecutor executor;
      rclcpp::executors::SingleThreadedExecutor executor;
 	   printf("ok here two\n");
      executor.add_node(node);
 	   printf("ok here three\n");
-
      auto spinner = std::thread([&executor]() { executor.spin(); });
-
 #endif
 
-
-
-
-#define LKIN
-
-#ifdef LKIN 
-	   printf("ok here 1\n");
-   // get all kinematics parameters from the move_group node
-    auto parameters_client = std::make_shared<rclcpp::SyncParametersClient>(node, "/move_group");
-	   printf("ok here 2\n");
-    auto plist = parameters_client->load_parameters("kinematics.yaml",  std::chrono::seconds(1)  );
-
-    //printf("%s\n", plist.parameters );
-     /*
-    while (!parameters_client->wait_for_service( std::chrono::seconds(1) )) {
-      if (!rclcpp::ok()) {
-        RCLCPP_ERROR(node->get_logger(), "Interrupted while waiting for the service. Exiting.");
-        rclcpp::shutdown();
-#ifdef SPIN
-        spinner.join();
-#endif	
-      }
-      RCLCPP_INFO(node->get_logger(), "service not available, waiting again...");
-    }
-    */
-
-
-
-	   printf("ok here 3\n");
-    //executor.remove_node(node);
-    rcl_interfaces::msg::ListParametersResult parameter_list = parameters_client->list_parameters({"robot_description_kinematics"},15);
-	   printf("ok here 3 1\n");
-    //printf("names %s\n", (char *) parameter_list.names );
-
-    //auto parameters = parameters_client->get_parameters(parameter_list.names);
-    //printf("names %s\n",  (char *) parameter_list.names );
-    // set the parameters to our node
-	   printf("ok here 4\n");
-    //node->set_parameters(parameters);
-	   printf("ok here 5\n");
-#endif
-
-
-
-
-
+ 
   // Create the MoveIt MoveGroup Interface
-  using moveit::planning_interface::MoveGroupInterface;
   auto move_group_interface = MoveGroupInterface(node, "arm");
-  //auto move_group_interface = MoveGroupInterface(node, "panda_arm");
-
-
-  // Set a target Pose
-  auto const target_pose = []{
-    geometry_msgs::msg::Pose msg;
-    msg.orientation.x = 0.434;
-    msg.orientation.y = 0.237;
-    msg.orientation.z = 0.417;
-    msg.orientation.w = 0.763;
-    msg.position.x = -1.514;
-    msg.position.y =  0.806;
-    msg.position.z =  3.206;
-    return msg;
-  }();
-
-   auto const ptarget_pose = [] {
-    geometry_msgs::msg::Pose msg;
-    msg.orientation.w = 1.0;
-    msg.position.x = 0.28;
-    msg.position.y = -0.2;
-    msg.position.z = 0.5;
-    return msg;
-  }();
-
-
-  /*
-   moveit::core::RobotState start_state(*move_group_interface.getCurrentState());
-   move_group_interface.setStartState(start_state);
-  //const moveit::core::JointModelGroup* joint_model_group =
-  //  move_group.getCurrentState()->getJointModelGroup("arm");
-  // start_state.setFromIK(joint_model_group, start_pose2);
-  //move_group_interface.setStartState(target_pose);
-
-   */
-
-//  for (int i=0; i < 100000; i++ );
-
-
-  printf("Before set target \n");
-  //move_group_interface.setPoseTarget(target_pose);
-  //move_group_interface.setPoseTarget(target_pose, "eef_link");
-  //move_group_interface.setApproximateJointValueTarget(target_pose, "panda_link8");
-  move_group_interface.setApproximateJointValueTarget(target_pose);
-  //move_group_interface.setRPYTarget(1.5,0.017, 0.017); 
-  //move_group_interface.setNamedTarget("pose1");
-  printf("After set target \n");
-
   
-  // Create a plan to that target pose
-  auto const [success, plan] = [&move_group_interface]{
-    moveit::planning_interface::MoveGroupInterface::Plan msg;
-    auto const ok = static_cast<bool>(move_group_interface.plan(msg));
-    return std::make_pair(ok, msg);
-  }();
+
+   std::vector<float> pos{-1.514, 0.806, 3.206}; std::vector<float> ang{0.434, 0.237, 0.417, 0.763};
+   plan_move (move_group_interface, pos, ang );
   
-  // Execute the plan
-  if(success) {
-    move_group_interface.execute(plan);
-  } else {
-    RCLCPP_ERROR(logger, "Planing failed!");
-  }
-  
-  // Shutdown ROS
+   std::vector<float> pos1{0.192, 3.065, 0.852}; std::vector<float> ang1{0.026, 0.000, 0.008, 1.0};
+   plan_move (move_group_interface, pos1, ang1 );
+ 
+   // Shutdown ROS
   rclcpp::shutdown();
 
 #ifdef SPIN
